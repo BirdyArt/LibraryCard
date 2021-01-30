5868cd7b50dc179533cb1770cd904a1a5efd6042
BirdyArt <asobolev91@gmail.com>
2021-01-29 17:15:53 -0800

    Constants added

--- frontend/src/actions/cards.js
+++ frontend/src/actions/cards.js
@@ -1,10 +1,11 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 export const getCards = () => async (dispatch) => {
   try {
     const { data } = await api.fetchCards();
 
-    dispatch({ type: 'FETCH_ALL', payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -14,7 +15,7 @@ export const createCard = (card) => async (dispatch) => {
   try {
     const { data } = await api.createCard(card);
 
-    dispatch({ type: 'CREATE', payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -24,7 +25,7 @@ export const updateCard = (id, card) => async (dispatch) => {
   try {
     const { data } = await api.updateCard(id, card);
 
-    dispatch({ type: 'UPDATE', payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -34,7 +35,7 @@ export const deleteCard = (id) => async (dispatch) => {
   try {
     await api.deleteCard(id);
 
-    dispatch({ type: 'DELETE', payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error.message);
   }
