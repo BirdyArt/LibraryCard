b2925b3ba2fcea9000ac34e87652fabfd2f976ef
BirdyArt <asobolev91@gmail.com>
2021-04-17 11:45:08 -0700

    Added Netlify.toml

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 
 const store = createStore(
   allReducer,
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(applyMiddleware(thunk))
   );
 
 ReactDOM.render(
